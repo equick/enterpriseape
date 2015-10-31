5b43305394aa1742404c0bc443f30caf142ef9a1
equick <edwardquick@hotmail.com>
2015-10-31 08:50:04 +0000

    added csv-uploader

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151030203217) do
+ActiveRecord::Schema.define(version: 20151031080614) do
+
+  create_table "companies", force: :cascade do |t|
+    t.string   "name"
+    t.string   "manager"
+    t.string   "status"
+    t.integer  "terms"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "invoices", force: :cascade do |t|
     t.datetime "date"
